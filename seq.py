4709bf512922589b70623b7674beb9ec8e8c5933
kieu <phamthikieu2001gtd@gmail.com>
2023-01-30 17:11:12 +0700

    commit 3

--- seq.py
+++ seq.py
@@ -6,7 +6,7 @@ print('Item 0 is', shoplist[0])
 print('Item 1 is', shoplist[1])
 print('Item 2 is', shoplist[2])
 print('Item 3 is', shoplist[3])
-print('Item -1 is', shoplist[-1])
+print('Item - is', shoplist[-1])
 print('Item -4 is', shoplist[-4])
 
 # Slicing on a list #
